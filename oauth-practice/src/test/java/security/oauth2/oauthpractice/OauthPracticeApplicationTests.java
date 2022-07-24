405453eb63cefe0338281c0e3398bed0a9a0aa3d
xi_jjun <rlawowns97@naver.com>
2022-07-25 02:28:30 +0900

    feat: spring oauth practice init

---
+++ oauth-practice/src/test/java/security/oauth2/oauthpractice/OauthPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package security.oauth2.oauthpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
